bb5410aeac1d126ea48b4d695ab93155172d1a74
thehamzaihsan <hamzaihsanpakistani@gmail.com>
2025-10-26 01:46:07 +0500

    gamescreen font changes

--- components/screens/GameScreen.tsx
+++ components/screens/GameScreen.tsx
@@ -637,6 +637,7 @@ const styles = StyleSheet.create({
   },
   nextButtonText: { color: "#fff", fontWeight: "bold" },
    levelTitle: {
+    fontFamily: "Pacifico",
     position: "absolute",
     left: 0,
     right: 0,
